4d65efd21369fb87f49b4bc3697e66d51536a7ce
Jai <jaikishannayak@gmail.com>
2026-03-24 12:47:07 +0530

    4: Implement session management with refresh tokens and add logout functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -13,6 +13,13 @@ authRouter.post("/register", authController.register)
 
 
 /**
+ * POST /api/auth/login
+ */
+authRouter.post("/login", authController.login)
+
+
+
+/**
  * GET /api/auth/get-me
  */
 authRouter.get("/get-me", authController.getMe)
@@ -21,6 +28,18 @@ authRouter.get("/get-me", authController.getMe)
  * GET /api/auth/refresh-token
  */
 authRouter.get("/refresh-token", authController.refreshToken)
+
+
+/**
+ * GET /api/auth/logout
+ */
+authRouter.get("/logout", authController.logout)
+
+
+/**
+ * GET /api/auth/logout-all
+ */
+authRouter.get("/logout-all", authController.logoutAll)
 
 
 export default authRouter;
